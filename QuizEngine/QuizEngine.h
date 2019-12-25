068e9118e9adf54fda074c98efc3927fce422bb2
Codesworth <codezworth@gmail.com>
2019-12-25 13:28:56 +0000

    Initial Commit

---
+++ QuizEngine/QuizEngine.h
@@ -0,0 +1,19 @@
+//
+//  QuizEngine.h
+//  QuizEngine
+//
+//  Created by Shadrach Mensah on 25/12/2019.
+//  Copyright © 2019 Shadrach Mensah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuizEngine.
+FOUNDATION_EXPORT double QuizEngineVersionNumber;
+
+//! Project version string for QuizEngine.
+FOUNDATION_EXPORT const unsigned char QuizEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuizEngine/PublicHeader.h>
+
+
